cd2fec7f4874de4ecfd574456b0546a07ab96741
electricmind <buy@neural.ru>
2016-01-15 03:05:54 +0300

    Implement notification as a player control

--- src/main/ru/wordmetrix/dreamcrammer/playerservice.scala
+++ src/main/ru/wordmetrix/dreamcrammer/playerservice.scala
@@ -1,192 +1,237 @@
 package ru.wordmetrix.dreamcrammer
 
-import android.graphics.{BitmapFactory, Bitmap}
-import android.support.v4.app.{NotificationManagerCompat, NotificationCompat}
+import android.app.{PendingIntent, Service}
+import android.content.Intent
+import android.graphics.{Bitmap, BitmapFactory}
+import android.os.{Binder, Handler, HandlerThread, IBinder, Message, Process}
 import android.support.v4.app.NotificationCompat.{BigPictureStyle, WearableExtender}
+import android.support.v4.app.{NotificationCompat, NotificationManagerCompat}
+import android.widget.Toast
+import ru.wordmetrix._
+import ru.wordmetrix.dreamcrammer.PlayerService._
+import ru.wordmetrix.dreamcrammer.db._
 
 import scala.annotation.tailrec
-import scala.util.{Try, Random}
-import java.io._
+import scala.util.{Random, Try}
 
-import android.content.Intent
-import android.media.{MediaPlayer, AudioManager}
-import android.app.{PendingIntent, IntentService, Service}
-import android.widget.Toast
-import android.os.{Binder, IBinder, AsyncTask, Handler, HandlerThread, Process, Message}
-
-import ru.wordmetrix.dreamcrammer.db._
-import ru.wordmetrix._
-
-class PlayerBinder(val service : PlayerService) extends Binder {
-    def getService() : PlayerService = {
-        return service
-    }
+class PlayerBinder(val service: PlayerService) extends Binder {
+  def getService(): PlayerService = {
+    return service
+  }
 }
 
-class PlayerService extends Service with PlayerBase//IntentService("DictLearnService")
-{
-  var stop : Boolean = true
-  var suspended : Boolean = false
-  var lock : AnyRef = new Object
+object PlayerService {
+
+  sealed trait PlayerServiceMessage extends Serializable
+
+  case object PlayerServiceMessageStop extends PlayerServiceMessage
+
+  case object PlayerServiceMessageStart extends PlayerServiceMessage
+
+  case object PlayerServiceMessagePause extends PlayerServiceMessage
+
+  case object PlayerServiceMessageResume extends PlayerServiceMessage
+
+  case object PlayerServiceMessageDefault extends PlayerServiceMessage
+
+  object NotificationIds extends Enumeration {
+    val Main = Value
+  }
+
+}
+
+class PlayerService
+  extends Service with PlayerBase {
+
+  //IntentService("DictLearnService")
+  var stop: Boolean = true
+  var suspended: Boolean = false
+  var lock: AnyRef = new Object
   var callbacks = List[Int => Unit]()
 
-  val binder : IBinder  = new PlayerBinder(this)
-  val handler : Handler = new Handler();
+  val binder: IBinder = new PlayerBinder(this)
+  val handler: Handler = new Handler();
 
   val history = new java.util.ArrayList[Int]()
   var history_size = 0
 
-  var servicehandler : Option[Handler] = None
+  var servicehandler: Option[Handler] = None
 
   override
-  def onBind(intent : Intent) : IBinder = binder
+  def onBind(intent: Intent): IBinder = binder
 
-  def status() : (Boolean, Boolean) = (stop, suspended)
+  def status(): (Boolean, Boolean) = (stop, suspended)
 
-  def pause() : Unit = {
-      suspended = true
+  def pause(): Unit = {
+    notificationManager.notify(NotificationIds.Main.id, mainNotificationBuilder(true).build())
+    suspended = true
   }
 
-  def resume() : Unit = {
-      suspended = false
+  def resume(): Unit = {
+    notificationManager.notify(NotificationIds.Main.id, mainNotificationBuilder(false).build())
+    suspended = false
   }
 
-  def exit() : Unit = {
-      stop = true
+  def exit(): Unit = {
+    notificationManager.notify(NotificationIds.Main.id, mainNotificationBuilder(true).build())
+    stop = true
   }
 
-  def seen(callback : Int => Unit) : Unit = {
-      callbacks = callback :: callbacks
-      println("callbacks :: " + callbacks)
+  def seen(callback: Int => Unit): Unit = {
+    callbacks = callback :: callbacks
+    println("callbacks :: " + callbacks)
   }
 
-  lazy val db : DB = new SQLiteAndroid(this, "taylor.db", true)
+  lazy val db: DB = new SQLiteAndroid(this, "taylor.db", true)
   //lazy val convertors =  new Convertors()(db)
   //import convertors._
 
-  override
-  def onCreate() : Unit = {
 
-    val playerPendingIntent: PendingIntent = PendingIntent.getActivity(
-      PlayerService.this,
-      0,
-      new Intent(PlayerService.this, classOf[Player]) {
-        //putExtra("word_ids", queryPlayerIds())
-      },
-      0)
+  def mainNotificationBuilder(isResume: Boolean) = {
+    var ids = 12 //Iterator.from(12)
+
+    trait PlayerServiceIntent {
+      protected def activity(intent: Intent) =
+        PendingIntent.getActivity(PlayerService.this, { ids += 1; ids}, intent, PendingIntent.FLAG_CANCEL_CURRENT)
+
+      protected def service(intent: Intent) =
+        PendingIntent.getService(PlayerService.this, { ids += 1; ids}, intent, PendingIntent.FLAG_CANCEL_CURRENT)
+
+      protected def intent(message: PlayerServiceMessage) = new Intent(PlayerService.this, classOf[PlayerService]) {
+        putExtra("message", message)
+      }
+    }
+
+
+    object PlayerServiceIntentMessage extends PlayerServiceIntent {
+      import scala.collection.JavaConversions._
+      def apply(message: PlayerServiceMessage) = message match {
+        case PlayerServiceMessageStart =>
+          activity(new Intent(PlayerService.this, classOf[Player]) {
+            putExtra("word_ids", Array() ) //Array[java.lang.Integer]()
+          })
+        case message =>
+          service(intent(message))
+      }
+    }
 
     val notificationBuilder: NotificationCompat.Builder =
       new NotificationCompat.Builder(PlayerService.this)
         .setSmallIcon(R.drawable.play)
-        .setContentTitle("Player")
-        .setContentText("Start player")
-        .setStyle(new NotificationCompat.InboxStyle()
+        .setContentTitle("English Words")
+        .setContentText(s"The thing plays flashcards, $history_size messages has played")
+//        .setStyle(new NotificationCompat.InboxStyle()
 //          .addLine("Alex Faaborg   Check this out")
 //          .addLine("Jeff Chang   Launch Party")
 //          .setSummaryText("johndoe@gmail.com")
-        )
+//        )
+//        .setStyle(new NotificationCompat.BigTextStyle().bigText("The thing plays flashcards"))
         .setGroup("wordnotification")
         .setOngoing(true)
         .setPriority(NotificationCompat.PRIORITY_MAX)
         .setGroupSummary(true)
-        //setContentIntent(playerPendingIntent).
-        .extend(
-          new WearableExtender()
-            .addAction(
-              new NotificationCompat.Action.Builder(
-                 R.drawable.play,
-                 s"""Start Player""",
-                 playerPendingIntent).build())
-            .addPage(
-                new NotificationCompat.Builder(PlayerService.this)
-                  .setStyle(
-                     new NotificationCompat.BigTextStyle()
-                        .setBigContentTitle("dasdasd")
-                        .bigText("sadsa"))
-                        .build()))
+        .setContentIntent(PlayerServiceIntentMessage(PlayerServiceMessageStart))
+        .addAction(R.drawable.exit, "Exit", PlayerServiceIntentMessage(PlayerServiceMessageStop))
 
-    notificationManager.notify(notifyId.next(), notificationBuilder.build())
+    if (isResume)
+      notificationBuilder.addAction(R.drawable.restart, "Resume", PlayerServiceIntentMessage(PlayerServiceMessageResume))
+    else notificationBuilder.addAction(R.drawable.pause, "Pause", PlayerServiceIntentMessage(PlayerServiceMessagePause))
+  }
 
-    servicehandler = Some(new Handler (new HandlerThread("ServiceStartArguments",  Process.THREAD_PRIORITY_BACKGROUND) { start() }.getLooper()) {
+  override
+  def onCreate(): Unit = {
+    notificationManager.notify(NotificationIds.Main.id, mainNotificationBuilder(false).build())
 
-        @tailrec
-        def scan(words : List[Word])(f : Word => Word) : List[Word] = if (stop) words else if (suspended) { Thread.sleep(1); scan(words)(f) } else words match {
-          case x :: xs => scan( xs :+ f(x))(f)
-          case List() => List()
-        }
+    servicehandler = Some(new Handler(new HandlerThread("ServiceStartArguments", Process.THREAD_PRIORITY_BACKGROUND) {
+      start()
+    }.getLooper()) {
 
-        @tailrec
-        def scanhmm2(word1 : Word, word2 : Word)(f : Word => Word) : List[Word] = if (stop) List() else if (suspended) { Thread.sleep(1); scanhmm2(word1,word2)(f) } else {
-            scanhmm2(word2, {
-                val word : Word = hmm(word1, word2) orElse hmm(word2) orElse hmm() getOrElse (new Word(1)(db))
-                log("word : %s",word)
-                word.track match {
-                    case NoData | NeverData => word
-                    case SomeData(x) => f(word)
-                }
-            })(f)
-        }
+      @tailrec
+      def scan(words: List[Word])(f: Word => Word): List[Word] = if (stop) words
+      else if (suspended) {
+        Thread.sleep(1);
+        scan(words)(f)
+      } else words match {
+        case x :: xs => scan(xs :+ f(x))(f)
+        case List() => List()
+      }
+
+      @tailrec
+      def scanhmm2(word1: Word, word2: Word)(f: Word => Word): List[Word] = if (stop) List()
+      else if (suspended) {
+        Thread.sleep(1);
+        scanhmm2(word1, word2)(f)
+      } else {
+        scanhmm2(word2, {
+          val word: Word = hmm(word1, word2) orElse hmm(word2) orElse hmm() getOrElse (new Word(1)(db))
+          log("word : %s", word)
+          word.track match {
+            case NoData | NeverData => word
+            case SomeData(x) => f(word)
+          }
+        })(f)
+      }
 
 
-        object hmm {
-            def query(q1: String, q2 : String, w1 : Int, w2 : Int) : Option[Word] = {
-                db.query(q1, x => x.columnInt(0), w1, w2) headOption match {
-                     case Some(x) => db.query(q2, x => new Word(x.columnInt(0))(db), x, w1, w2) match {
-                         case Stream() => None
-                         case xs => Some(xs(scala.util.Random.nextInt(xs.size)))
-                     }
-
-                     case None => None
-                }
+      object hmm {
+        def query(q1: String, q2: String, w1: Int, w2: Int): Option[Word] = {
+          db.query(q1, x => x.columnInt(0), w1, w2) headOption match {
+            case Some(x) => db.query(q2, x => new Word(x.columnInt(0))(db), x, w1, w2) match {
+              case Stream() => None
+              case xs => Some(xs(scala.util.Random.nextInt(xs.size)))
             }
 
-            def apply()  = query("select max(word_frequency) from word", "select word_id,word_frequency from word where word_frequency > abs(random()) % ?", 0, 1)
-
-            def apply(w1 : Word) = query("select max(hmm1_frequency) from hmm1 where hmm1_word1_id = ?","select hmm1_emit_id,hmm1_frequency from hmm1 where hmm1_frequency > abs(random()) % ? and hmm1_word1_id=?",w1.id,1)
-
-            def apply(w1 : Word, w2 : Word) = query("select max(hmm2_frequency) from hmm2 where hmm2_word1_id = 3 and hmm2_word2_id=4", "select hmm2_emit_id,hmm2_frequency from hmm2 where hmm2_frequency > abs(random()) % 2 and hmm2_word1_id=3 and hmm2_word2_id=4 order by hmm2_frequency", w1.id, w2.id)
+            case None => None
+          }
         }
 
+        def apply() = query("select max(word_frequency) from word", "select word_id,word_frequency from word where word_frequency > abs(random()) % ?", 0, 1)
 
-        override
-        def handleMessage(msg : Message) : Unit = {
-            stop = false
-            suspended = false
+        def apply(w1: Word) = query("select max(hmm1_frequency) from hmm1 where hmm1_word1_id = ?", "select hmm1_emit_id,hmm1_frequency from hmm1 where hmm1_frequency > abs(random()) % ? and hmm1_word1_id=?", w1.id, 1)
 
-            lock.synchronized {
-                scan(scala.util.Random.shuffle(msg.obj.asInstanceOf[List[Word]]))(word => {
-                    handler.post(new Runnable {
-                        def run() : Unit = callbacks.map(_(word.id))
-                    })
+        def apply(w1: Word, w2: Word) = query("select max(hmm2_frequency) from hmm2 where hmm2_word1_id = 3 and hmm2_word2_id=4", "select hmm2_emit_id,hmm2_frequency from hmm2 where hmm2_frequency > abs(random()) % 2 and hmm2_word1_id=3 and hmm2_word2_id=4 order by hmm2_frequency", w1.id, w2.id)
+      }
 
-                    handler.post( new Runnable {
-                        def run() : Unit =  Toast.makeText(PlayerService.this, "Player: %s".format(word.value), Toast.LENGTH_SHORT).show();
-                    })
 
-                    history.add(word.id)
-                    history_size = history_size + 1
+      override
+      def handleMessage(msg: Message): Unit = {
+        stop = false
+        suspended = false
 
-                    val vocabularyPendingIntent: PendingIntent = PendingIntent.getActivity(
-                      PlayerService.this,
-                      word.id,
-                      new Intent(PlayerService.this, classOf[Vocabulary]) {
-                        putExtra("word_id", word.id)
-                      },
-                      0)
+        lock.synchronized {
+          scan(scala.util.Random.shuffle(msg.obj.asInstanceOf[List[Word]]))(word => {
+            handler.post(new Runnable {
+              def run(): Unit = callbacks.map(_ (word.id))
+            })
 
-                    val stopPendingIntent: PendingIntent = PendingIntent.getService(
-                      PlayerService.this,
-                      0,
-                      new Intent(PlayerService.this, classOf[PlayerService]) {
-                        putExtra("doStop", true)
-                      },
-                      0)
+            handler.post(new Runnable {
+              def run(): Unit = Toast.makeText(PlayerService.this, "Player: %s".format(word.value), Toast.LENGTH_SHORT).show();
+            })
 
-                    def bitmap = word.pictures.view.take(0).flatMap(_.bodyOption).flatMap(body => Try(
-                      Bitmap.createScaledBitmap(BitmapFactory.decodeByteArray(body, 0, body.size), 256, 256, true)).toOption)
+            history.add(word.id)
+            history_size = history_size + 1
 
-                    val notificationBuilder: NotificationCompat.Builder =
-                      new NotificationCompat.Builder(PlayerService.this)
+            val vocabularyPendingIntent: PendingIntent = PendingIntent.getActivity(
+              PlayerService.this,
+              word.id,
+              new Intent(PlayerService.this, classOf[Vocabulary]) {
+                putExtra("word_id", word.id)
+              },
+              0)
+
+            val stopPendingIntent: PendingIntent = PendingIntent.getService(
+              PlayerService.this,
+              10,
+              new Intent(PlayerService.this, classOf[PlayerService]) {
+                putExtra("message", PlayerServiceMessageStop)
+              },
+              0)
+
+            def bitmap = word.pictures.view.take(0).flatMap(_.bodyOption).flatMap(body => Try(
+              Bitmap.createScaledBitmap(BitmapFactory.decodeByteArray(body, 0, body.size), 256, 256, true)).toOption)
+
+            val notificationBuilder: NotificationCompat.Builder =
+              new NotificationCompat.Builder(PlayerService.this)
                         .setSmallIcon(R.drawable.play)
                         .setContentTitle(word.value)
                         .setContentText(word.ipa getOrElse word.descriptions.mkString("\n"))
@@ -223,24 +268,24 @@ class PlayerService extends Service with PlayerBase//IntentService("DictLearnSer
                                      .bigText(word.phrases.map(_.value).mkString("\n")))
                                  .build()))
 
-                    notificationManager.notify(word.id, notificationBuilder.build())
+            notificationManager.notify(word.id, notificationBuilder.build())
 
-                    handler.postDelayed(new Runnable() {
-                      def run() {
-                         notificationManager.cancel(word.id)
-                      }
-                    }, 40000)
+            handler.postDelayed(new Runnable() {
+              def run() {
+                notificationManager.cancel(word.id)
+              }
+            }, 40000)
 
-                    Thread.sleep(500)
-                    play(word)
-                    Thread.sleep(2000)
-                    play(word)
-                    word
-                })
-            }
-
-            stopSelf(msg.arg1)
+            Thread.sleep(500)
+            play(word)
+            Thread.sleep(2000)
+            play(word)
+            word
+          })
         }
+
+        stopSelf(msg.arg1)
+      }
     })
   }
 
@@ -249,29 +294,36 @@ class PlayerService extends Service with PlayerBase//IntentService("DictLearnSer
   lazy val notifyId = Iterator.from(Random.nextInt / 2)
 
   override
-  def onStartCommand(intent : Intent, flags : Int, startId : Int) : Int = {
-      if (intent.getBooleanExtra("doStop", false)) {
-        stop = true
-      } else if (stop) {
-          Toast.makeText(this, "Service has started", Toast.LENGTH_SHORT).show()
-          servicehandler match {
-              case None => {}
-              case Some(servicehandler) => {
-                  val msg : Message = servicehandler.obtainMessage();
-                  msg.arg1 = startId
-                  // ticker: application failed in this place with null-exception
-                  msg.obj = intent.getIntArrayExtra("word_ids").toList.map(new Word(_)(db))
-                  servicehandler.sendMessage(msg)
-              }
-           }
-      } else {
-          Toast.makeText(this, "Pronounciation has already started playing, new intention ignored", Toast.LENGTH_SHORT).show()
-      }
-      Service.START_STICKY
+  def onStartCommand(intent: Intent, flags: Int, startId: Int): Int = {
+    Option(intent.getSerializableExtra("message")) match {
+      case Some(PlayerServiceMessagePause) =>
+        pause()
+
+      case Some(PlayerServiceMessageResume) =>
+        resume()
+
+      case Some(PlayerServiceMessageStop) =>
+        exit()
+
+      case x if (stop) =>
+        Toast.makeText(this, "Service has started", Toast.LENGTH_SHORT).show()
+        servicehandler match {
+          case None =>
+          case Some(servicehandler) =>
+            val msg: Message = servicehandler.obtainMessage();
+            msg.arg1 = startId
+            // ticker: application failed in this place with null-exception
+            msg.obj = intent.getIntArrayExtra("word_ids").toList.map(new Word(_)(db))
+            servicehandler.sendMessage(msg)
+        }
+      case _ =>
+        Toast.makeText(this, "Pronounciation has already started playing, new intention ignored", Toast.LENGTH_SHORT).show()
+    }
+    Service.START_STICKY
   }
 
   override
-  def onDestroy() : Unit = {
+  def onDestroy(): Unit = {
     Toast.makeText(this, "Service has done", Toast.LENGTH_SHORT).show();
   }
 }
