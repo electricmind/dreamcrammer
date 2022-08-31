90feb85d887fb35ec1e5754f79e4dac5b40bbba0
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-31 21:17:55 +0800

    Delete src/main directory

--- src/main/ru/wordmetrix/dreamcrammer/db/trackdictionary.scala
+++
@@ -1,9 +0,0 @@
-package ru.wordmetrix.dreamcrammer.db
-
-import java.io._
-import java.net._
-
-object TrackDictionary extends Track {
-    def get(url : String) : Option[Array[Byte]] = ("http://dictionary.reference.com/browse/%s".format(url) :+: Page("""a\s*class="speaker"\s*href="([^"]*)"""".r, x=>x.subgroups(0))) :+: Data()
-}
-
